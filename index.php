1af8dc13726752d9f2e4a551c26f139962c4d0a8
MuhamadJajuni <muhamadjajuni1@gmail.com>
2023-07-03 11:49:35 +0700

    feat: init pertama

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Junior Web Developer</title>
+</head>
+<body>
+    <p>Kalimat ini dibuat dengan HTML</p>
+
+    <?php 
+    echo "<h1>Kalimat ini dibuat dengan PHP</h1>";
+
+    ?>
+</body>
+</html>
